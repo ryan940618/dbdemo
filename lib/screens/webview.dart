f86ebae1b8348a565e64a5822e5fff2d04df9214
ryan940618 <ryan940618@gmail.com>
2026-01-05 17:42:57 +0800

    Added webview page for spreadsheet

---
+++ lib/screens/webview.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String url;
+  final String title;
+
+  const WebViewPage({Key? key, required this.url, required this.title}) : super(key: key);
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)), 
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
