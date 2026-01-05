614acc173ff95d2e4c84def1bde1c049c03e1883
ryan940618 <ryan940618@gmail.com>
2026-01-05 17:47:19 +0800

    Added routes and firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screens/mainmenu.dart';
 import 'screens/order.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
+import 'screens/login.dart';
+import 'screens/admin.dart';
+import 'screens/about.dart';
+import 'screens/team.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +30,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/mainmenu': (context) => MainMenu(),
         '/order': (context) => OrderPage(),
+        '/login': (context) => LoginPage(),
+        '/admin': (context) => AdminPage(),
+        '/team': (context) => const TeamPage(),
+        '/about': (context) => const AboutPage(),
       },
     );
   }
